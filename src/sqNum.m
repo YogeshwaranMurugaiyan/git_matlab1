e20e1a086a9a2131541be09d4e0afd009bd51334
yogeshwaran <yogeshwaran.ma@gmail.com>
2022-04-03 11:34:52 +0200

    first commit

---
+++ src/sqNum.m
@@ -0,0 +1,5 @@
+function out = sqNum(number)
+
+out = number * number
+
+end
---
+++ test/testCalc.m
@@ -0,0 +1,12 @@
+classdef testCalc < matlab.unittest.TestCase
+    %TESTCALC Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    methods(Test)
+        function testSquare(testCase)
+            %TESTCALC Construct an instance of this class
+            %   Detailed explanation goes here
+            testCase.verifyEqual(sqNum(2), 4);
+        end
+    end
+end
